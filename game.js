ec9b3e7f47c63a6d9566dfb2a558e5bd36fd8838
Aaron Liu <aaron.j.liu@gmail.com>
2022-05-17 19:06:46 -0400

    Added UI that interacts with rps game... Added scoreboard to keep track of score Added Welcome section + color + text Added game result to appear on screen

--- game.js
+++ game.js
@@ -15,41 +15,86 @@ function computerPlay() {
 
 function decideWinner(playerSelection, computerSelection) {
     if(playerSelection === 0) {
-        if(computerSelection === 0) return "Draw! Both picked rock.";
-        else if(computerSelection === 1) return "You lose! Paper beats rock.";
-        else return "You win! Rock beats scissors.";
+        if(computerSelection === 0) {
+            result.textContent = "Draw! Both picked rock.";
+        } 
+        else if(computerSelection === 1) {
+            result.textContent = "You lose! Paper beats rock.";
+            computerScore.textContent = parseInt(computerScore.textContent)+1;
+        }
+        else {
+            result.textContent = "You win! Rock beats scissors.";
+            humanScore.textContent = parseInt(humanScore.textContent)+1;
+        }
     }
     else if(playerSelection === 1) {
-        if(computerSelection === 0) return "You win! Paper beats rock.";
-        else if(computerSelection === 1) return "Draw! Both picked paper.";
-        else return "You lose! Scissors beats paper.";
+        if(computerSelection === 0) {
+            result.textContent = "You win! Paper beats rock.";
+            humanScore.textContent = parseInt(humanScore.textContent)+1;
+        } 
+        else if(computerSelection === 1) {
+            result.textContent = "Draw! Both picked paper.";
+        } 
+        else {
+            result.textContent = "You lose! Scissors beats paper.";
+            computerScore.textContent = parseInt(computerScore.textContent)+1;
+        } 
     }
     else {
-        if(computerSelection === 0) return "You lose! Rock beats scissors.";
-        else if(computerSelection === 1) return "You win! Scissors beats paper.";
-        else return "Draw! Both picked scissors.";
+        if(computerSelection === 0) {
+            result.textContent = "You lose! Rock beats scissors.";
+            computerScore.textContent = parseInt(computerScore.textContent)+1;
+        }
+        else if(computerSelection === 1) {
+            result.textContent = "You win! Scissors beats paper.";
+            humanScore.textContent = parseInt(humanScore.textContent)+1;
+        }
+        else {
+            result.textContent = "Draw! Both picked scissors.";
+        }
     }
 }
 
-let playerResponse = prompt("Welcome to rock-paper-scissors. Enter your choice: ");
-playerResponse = playerResponse.toLowerCase();
-console.log("You chose " + playerResponse);
-if(playerResponse === "rock") {
-    playerResponse = 0;
-}
-else if(playerResponse === "paper") {
-    playerResponse = 1;
-}
-else if(playerResponse === "scissors") {
-    playerResponse = 2;
-}
-else {
-    playerResponse = 3;
+
+
+const buttons = document.querySelectorAll('button');
+const result = document.querySelector('.declare-winner');
+const computerScore = document.querySelector('#computer-score');
+const humanScore = document.querySelector('#human-score');
+
+for(let i=0; i<buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+        decideWinner(i, computerPlay());
+    });
+
 }
 
-if(playerResponse != 3) {
-    console.log(decideWinner(playerResponse, computerPlay()));
-}
-else {
-    console.log("Unvalid response");
-}
+// const rockButton = document.querySelector('#rock');
+// const paperButton = document.querySelector('#paper');
+// const scissorsButton = document.querySelector('#scissors');
+
+// rockButton.addEventListener('click', decideWinner(0, computerPlay()));
+// paperButton.addEventListener('click', decideWinner(1, computerPlay()));
+
+// let playerResponse = prompt("Welcome to rock-paper-scissors. Enter your choice: ");
+// playerResponse = playerResponse.toLowerCase();
+// console.log("You chose " + playerResponse);
+// if(playerResponse === "rock") {
+//     playerResponse = 0;
+// }
+// else if(playerResponse === "paper") {
+//     playerResponse = 1;
+// }
+// else if(playerResponse === "scissors") {
+//     playerResponse = 2;
+// }
+// else {
+//     playerResponse = 3;
+// }
+
+// if(playerResponse != 3) {
+//     console.log(decideWinner(playerResponse, computerPlay()));
+// }
+// else {
+//     console.log("Unvalid response");
+// }
