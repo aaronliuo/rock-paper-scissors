c3483779a0b3afb9a2899d4ab96ba05a2618532d
Aaron Liu <aaron.j.liu@gmail.com>
2022-05-17 20:31:35 -0400

    Edited result text message

--- game.js
+++ game.js
@@ -1,14 +1,5 @@
 function computerPlay() {
     let computerChoice = Math.floor(Math.random()*3);
-    if(computerChoice === 0) {
-        console.log("Computer chose rock");
-    }
-    else if(computerChoice === 1) {
-        console.log("Computer chose paper");
-    }
-    else {
-        console.log("Computer chose scissors");
-    }
 
     return computerChoice;
 }
@@ -19,34 +10,34 @@ function decideWinner(playerSelection, computerSelection) {
             result.textContent = "Draw! Both picked rock.";
         } 
         else if(computerSelection === 1) {
-            result.textContent = "You lose! Paper beats rock.";
+            result.textContent = "You lose! You picked rock and computer picked paper.";
             computerScore.textContent = parseInt(computerScore.textContent)+1;
         }
         else {
-            result.textContent = "You win! Rock beats scissors.";
+            result.textContent = "You win! You picked rock and computer picked scissors.";
             humanScore.textContent = parseInt(humanScore.textContent)+1;
         }
     }
     else if(playerSelection === 1) {
         if(computerSelection === 0) {
-            result.textContent = "You win! Paper beats rock.";
+            result.textContent = "You win! You picked paper and computer picked rock.";
             humanScore.textContent = parseInt(humanScore.textContent)+1;
         } 
         else if(computerSelection === 1) {
             result.textContent = "Draw! Both picked paper.";
         } 
         else {
-            result.textContent = "You lose! Scissors beats paper.";
+            result.textContent = "You lose! You picked paper and computer picked scissors.";
             computerScore.textContent = parseInt(computerScore.textContent)+1;
         } 
     }
     else {
         if(computerSelection === 0) {
-            result.textContent = "You lose! Rock beats scissors.";
+            result.textContent = "You lose! You picked scissors and computer picked rock.";
             computerScore.textContent = parseInt(computerScore.textContent)+1;
         }
         else if(computerSelection === 1) {
-            result.textContent = "You win! Scissors beats paper.";
+            result.textContent = "You win! You picked scissors and computer picked paper.";
             humanScore.textContent = parseInt(humanScore.textContent)+1;
         }
         else {
@@ -69,32 +60,3 @@ for(let i=0; i<buttons.length; i++) {
 
 }
 
-// const rockButton = document.querySelector('#rock');
-// const paperButton = document.querySelector('#paper');
-// const scissorsButton = document.querySelector('#scissors');
-
-// rockButton.addEventListener('click', decideWinner(0, computerPlay()));
-// paperButton.addEventListener('click', decideWinner(1, computerPlay()));
-
-// let playerResponse = prompt("Welcome to rock-paper-scissors. Enter your choice: ");
-// playerResponse = playerResponse.toLowerCase();
-// console.log("You chose " + playerResponse);
-// if(playerResponse === "rock") {
-//     playerResponse = 0;
-// }
-// else if(playerResponse === "paper") {
-//     playerResponse = 1;
-// }
-// else if(playerResponse === "scissors") {
-//     playerResponse = 2;
-// }
-// else {
-//     playerResponse = 3;
-// }
-
-// if(playerResponse != 3) {
-//     console.log(decideWinner(playerResponse, computerPlay()));
-// }
-// else {
-//     console.log("Unvalid response");
-// }
